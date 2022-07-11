a31a22495ace965cd1cb2a11df350a8eaac7ef1c
Harry Edgar <harryedgar@hotmail.co.uk>
2022-07-11 12:42:00 +0100

    initial commit

---
+++ main.rb
@@ -0,0 +1,27 @@
+
+class Tree(array)
+    def initialize
+        @array = array
+    end
+end
+
+class Node
+    def initialize
+        
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+array = [1, 2, 3, 4, 5, 6, 7]
