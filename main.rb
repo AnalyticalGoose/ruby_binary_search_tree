a7ac9a019eb298dff6bcc471941da94f62a586b0
Harry Edgar <harryedgar@hotmail.co.uk>
2022-07-11 22:06:17 +0100

    Add DFS algos

--- main.rb
+++ main.rb
@@ -53,13 +53,66 @@ class Tree
     end
 
     def find(value, root = @root)
-        return nil if root.nil? || root.data == value
+        return root if root.nil? || root.data == value
         root.data < value ? find(value, root.right) : find(value, root.left)
     end
 
     def find_leaf(node = @root)
         return node.data if node.left.nil?
         find_leaf(node.left)
+    end
+
+    def level_order(stack = [], order = [], root = @root)
+        stack << root.left unless root.left.nil?
+        stack << root.right unless root.right.nil?
+        order << root.data
+        
+        yield(root.data) if block_given?
+        return if stack.empty?
+
+        level_order(stack, order, stack.shift)
+        return order
+    end
+
+    def inorder(stack = [], order = [], root = @root)
+        return nil if root.nil?
+        yield(root.data) if block_given?
+        
+        inorder(stack, order, root.left)
+        order << root.data
+        inorder(stack, order, root.right)
+
+        return order
+    end
+
+    def preorder(stack = [], order = [], root = @root)
+        return nil if root.nil?
+        yield(root.data) if block_given?
+
+        order << root.data
+        preorder(stack, order, root.left)
+        preorder(stack, order, root.right)
+
+        return order
+    end
+
+    def postorder(stack = [], order = [], root = @root)
+        return nil if root.nil?
+        yield(root.data) if block_given?
+
+        postorder(stack, order, root.left)
+        postorder(stack, order, root.right)
+        order << root.data
+
+        return order
+    end
+
+    def height(node)
+        unless node.nil? || node == root
+            node = (node.instance_of?(Node) ? find(node.data) : find(node))
+        end 
+        return -1 if node.nil? 
+        [height(node.left), height(node.right)].max + 1
     end
 
     def pretty_print(node = root, prefix = '', is_left = true)
@@ -85,6 +138,8 @@ end
 
 arr = [1, 2, 3, 4, 5, 6, 7]
 bst = Tree.new(arr)
+
+p bst.height(2)
 
 puts bst.pretty_print
 # p bst
